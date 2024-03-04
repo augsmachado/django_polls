a7ba50e6332977e3ec38667a11501c17523b8858
Augs Machado <augustomachadoramos@gmail.com>
2024-03-04 17:06:33 -0300

    .

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,4 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("polls/", include("polls.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("polls/", include("polls.urls")),
+]
